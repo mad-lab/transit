342a3cef3f87867904e833556532506424c41d82
Michael DeJesus <mad@cs.tamu.edu>
2015-03-29 15:01:58 -0500

    Added copyright and license header to source files. Removed test files.

--- transit.py
+++ transit.py
@@ -1,3 +1,23 @@
+# Copyright 2015.
+#   Michael A. DeJesus, Chaitra Ambadipudi, and  Thomas R. Ioerger.
+#
+#
+#    This file is part of TRANSIT.
+#
+#    TRANSIT is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License.
+#
+#
+#    TRANSIT is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with TRANSIT.  If not, see <http://www.gnu.org/licenses/>.
+
+
 #importing wx files
 
 import sys
