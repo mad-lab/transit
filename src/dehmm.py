1b35dbeb4f8b9ede1da6d04484fdb0eab69c8232
Michael DeJesus <mad@cs.tamu.edu>
2015-12-20 15:59:07 -0600

    Normalizing to 100 when doing DEHMM

--- src/dehmm.py
+++ src/dehmm.py
@@ -93,10 +93,16 @@ def runDEHMM(wx, pubmsg, **kwargs):
     data = factors * data
 
     #O = numpy.round(numpy.sum(data,0))
-    ctrldata = numpy.round(numpy.mean(data[:N1,:],0)) + 1
-    expdata = numpy.round(numpy.mean(data[N1:,:],0)) + 1
-    jointdata = numpy.round((ctrldata+expdata)/2.0) + 1
+    combined_data = numpy.zeros((3, len(ctrldata)))
 
+    combined_data[0,:] = numpy.round(numpy.mean(data[:N1,:],0)) + 1
+    combined_data[1,:] = numpy.round(numpy.mean(data[N1:,:],0)) + 1
+    combined_data[2,:] = numpy.round((ctrldata+expdata)/2.0) + 1
+    factors = transit_tools.norm_to_target(combined_data, 100)
+    combined_data = factors * combined_data
+    ctrldata = combined_data[0,:]
+    expdata = combined_data[1,:]
+    jointdata = combined_data[2,:]
     
     #Control HMM
     print dehmm_prefix, "Running Control HMM"
