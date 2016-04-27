6a3ca508c1eb6c4b341713e19f50e7c18acf8ce8
Michael DeJesus <mad@cs.tamu.edu>
2016-04-27 13:44:02 -0500

    Fixed error in tn5gaps name

--- src/transit/analysis/__init__.py
+++ src/transit/analysis/__init__.py
@@ -9,7 +9,7 @@ import base
 
 import gumbel
 import example
-import globalruns
+import tn5gaps
 import binomial
 import griffin
 import resampling
@@ -24,7 +24,7 @@ methods["binomial"] = binomial.BinomialAnalysis()
 methods["griffin"] = griffin.GriffinAnalysis()
 methods["hmm"] = hmm.HMMAnalysis()
 methods["resampling"] = resampling.ResamplingAnalysis()
-methods["tn5gaps"] = globalruns.GlobalGumbelAnalysis()
+methods["tn5gaps"] = tn5gaps.Tn5GapsAnalysis()
 methods["rankproduct"] = rankproduct.RankProductAnalysis()
 
 
