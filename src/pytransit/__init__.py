ceb28a1ded817a6f7aefd8e6472dc0e1012fbd25
Michael DeJesus <mad@cs.tamu.edu>
2016-08-22 20:43:41 -0500

    Fixed imports for package

--- src/pytransit/__init__.py
+++ src/pytransit/__init__.py
@@ -1,6 +1,11 @@
-__all__ = []
 
-#__all__ = ["transit_tools", "tnseq_tools", "norm_tools", "stat_tools"]
+__all__ = ["transit_tools", "tnseq_tools", "norm_tools", "stat_tools"]
+
+
+import transit_tools
+import tnseq_tools
+import norm_tools
+import stat_tools
 
 __version__ = "v2.0.2"
 prefix = "[TRANSIT]"
