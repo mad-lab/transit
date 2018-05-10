ec91352e2e6226a44560875a903e1ab5b82cc33a
Michael A. DeJesus <mad@cs.tamu.edu>
2018-05-10 16:18:42 -0500

    Reorganized exports to have structure like analysis methods

---
+++ src/pytransit/export/__init__.py
@@ -0,0 +1,16 @@
+#__all__ = []
+
+from os.path import dirname, basename, isfile
+import glob
+modules = glob.glob(dirname(__file__)+"/*.py")
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
+
+
+import combined_wig
+
+# EXPORT METHODS
+methods = {}
+methods["combined_wig"] = combined_wig.CombinedWigExport()
+
+
+
