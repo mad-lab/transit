ab191e6ebfaeaf4afe56f5434b521d80e62ece03
Michael A. DeJesus <mad@cs.tamu.edu>
2018-05-23 12:26:08 -0500

    Adding wxpython as a requirement from now on

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['setuptools', 'numpy', 'scipy', 'pillow', 'matplotlib'],
+    install_requires=['setuptools', 'numpy', 'scipy', 'pillow', 'matplotlib', 'wxpython'],
     
     #dependency_links = [
     #	"git+https://github.com/wxWidgets/wxPython.git#egg=wxPython"
