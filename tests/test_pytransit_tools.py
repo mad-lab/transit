8396e1505079c41d2ea7e76fadc6e899ae2f1e8e
Michael A DeJesus <mdejesus@rockefeller.edu>
2018-09-27 11:03:13 -0400

    Added test cases for input argument parsing function

--- tests/test_pytransit_tools.py
+++ tests/test_pytransit_tools.py
@@ -12,6 +12,7 @@ from transit_test import *
 import pytransit.norm_tools as norm_tools
 import pytransit.tnseq_tools as tnseq_tools
 import pytransit.stat_tools as stat_tools
+import pytransit.transit_tools as transit_tools
 
 
 
@@ -67,7 +68,7 @@ class TestTnSeqTools(TransitTestCase):
         self.assertEqual(len(types), 1)
         self.assertTrue("himar1" in types)
 
-
+#
 
     def test_normalization(self):
         N = len(all_data_list)
@@ -75,8 +76,49 @@ class TestTnSeqTools(TransitTestCase):
         norm_data,factors = norm_tools.normalize_data(data, "TTR")
         self.assertFalse((factors == numpy.ones(N)).all())
    
-    
-    
+#
+
+    def test_cleanargs_negative_arguments(self):
+        TEST_RAWARGS = ["test", "-p", "-10"]
+        args, kwargs = transit_tools.cleanargs(TEST_RAWARGS)
+        self.assertEqual(int(kwargs.get("p",0)), -10)
+
+#       
+ 
+    def test_cleanargs_flag_without_arguments(self):
+        TEST_RAWARGS = ["test", "-p"]
+        args, kwargs = transit_tools.cleanargs(TEST_RAWARGS)
+        self.assertTrue(kwargs.get("p",False))
+
+#
+
+    def test_cleanargs_positional_arguments(self):
+        TEST_RAWARGS = ["a", "b", "c", "-d", "1", "e"]
+        args, kwargs = transit_tools.cleanargs(TEST_RAWARGS)
+        self.assertEqual(args, ["a", "b", "c", "e"])
+
+#
+
+    def test_cleanargs_positional_arguments_w_quotes(self):
+        TEST_RAWARGS = ["a", "b", "c", "-d", "1", "test this"]
+        args, kwargs = transit_tools.cleanargs(TEST_RAWARGS)
+        self.assertEqual(args, ["a", "b", "c", "test this"])
+
+#
+
+    def test_cleanargs_flag_arguments_w_quotes(self):
+        TEST_RAWARGS = ["a", "b", "c", "-d", "1", "-p", "test this"]
+        args, kwargs = transit_tools.cleanargs(TEST_RAWARGS)
+        self.assertEqual(kwargs.get("p"), "test this")
+
+#
+
+    def test_cleanargs_flag_arguments_with_double_dash(self):
+        TEST_RAWARGS = ["a", "b", "c", "-d", "1", "--p", "test this"]
+        args, kwargs = transit_tools.cleanargs(TEST_RAWARGS)
+        self.assertFalse("p" in kwargs)
+        self.assertFalse("--p" in kwargs)
+        self.assertTrue("-p" in kwargs)
 
 
 
