21473e77d5e31cec4f9e93c88050eaf72f6ca286
Michael A DeJesus <mdejesus@rockefeller.edu>
2018-09-27 11:05:40 -0400

    Fixed bug that could happen with negative arguments passed to flags

--- src/pytransit/transit_tools.py
+++ src/pytransit/transit_tools.py
@@ -153,19 +153,62 @@ def dirname(filepath):
 
 
 def cleanargs(rawargs):
-    #TODO: Write docstring
+    """Returns a list and a dictionary with positional and keyword arguments.
+
+    -This function assumes flags must start with a "-" and and cannot be a 
+        number (but can include them).
+    
+    -Flags should either be followed by the value they want to be associated 
+        with (i.e. -p 5) or will be assigned a value of True in the dictionary.
+
+    -The dictionary will map flags to the name given minus ONE "-" sign in
+        front. If you use TWO minus signs in the flag name (i.e. --verbose), 
+        the dictionary key will be the name with ONE minus sign in front 
+        (i.e. {"-verbose":True}).
+    
+
+    Arguments:
+        rawargs (list): List of positional/keyword arguments. As obtained from
+                         sys.argv.
+
+    Returns:
+        list: List of positional arguments (i.e. arguments without flags),
+                in order provided.
+        dict: Dictionary mapping flag (key is flag minus the first "-") and
+                their values.
+
+    """
     args = []
     kwargs = {}
     count = 0
     # Loop through list of arguments
     while count < len(rawargs):
-        # If the current argument starts with "-"
+        # If the current argument starts with "-", then it's probably a flag
         if rawargs[count].startswith("-"):
-            if count + 1 < len(rawargs) and (not rawargs[count+1].startswith("-") or len(rawargs[count+1].split(" ")) > 1):
+            # Check if next argument is a number
+            try:
+                temp = float(rawargs[count+1])
+                nextIsNumber = True
+            except:
+                nextIsNumber = False
+
+            stillNotFinished = count + 1 < len(rawargs)
+            if stillNotFinished:
+                nextIsNotArgument = not rawargs[count+1].startswith("-")
+                nextLooksLikeList = len(rawargs[count+1].split(" ")) > 1
+            else:
+                nextIsNotArgument = True
+                nextLooksLikeList = False
+
+
+            # If still things in list, and they look like arguments to a flag, add them to dict 
+            if stillNotFinished and (nextIsNotArgument or nextLooksLikeList or nextIsNumber):
                 kwargs[rawargs[count][1:]] = rawargs[count+1]
                 count += 1
+            # Else it's a flag but without arguments/values so assign it True
             else:
                 kwargs[rawargs[count][1:]] = True
+        # Else, it's probably a positional arguement without flags
         else:
             args.append(rawargs[count])
         count += 1
