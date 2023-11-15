c52e3b0a7663cd914dd66f17dcda0d39a1e9bfad
ioerger2 <ioerger@cs.tamu.edu>
2023-11-15 10:47:24 -0600

    in HMM_conf.py, changed model of Saturation of 4 states to Beta distribution

--- src/HMM_conf.py
+++ src/HMM_conf.py
@@ -1,6 +1,8 @@
 import sys,numpy
 import scipy.stats
 
+STATES = "ES GD NE GA".split()
+
 # first pass...
 
 headers = []
@@ -27,7 +29,8 @@ for line in open(sys.argv[1]):
   all.append(consistency)
   data.append(w)
 
-print("# avg gene-level consistency of HMM states: %s" % (round(numpy.mean(all),4)))
+Info = ["# HMM confidence info:"]
+Info.append("# avg gene-level consistency of HMM states: %s" % (round(numpy.mean(all),4)))
 
 cons = numpy.mean(all)
 
@@ -35,59 +38,101 @@ IDs = [x[0] for x in data]
 
 meanSats,meanNZmeans = {},{}
 stdSats,stdNZmeans = {},{}
+SatParams = {}
+NZmeanParams = {}
 
-print("# state posterior probability distributions:")
+Info.append("# state posterior probability distributions:")
 for st in ["ES","GD","NE","GA"]:
   sub = list(filter(lambda id: Calls[id]==st,IDs))
-  meanSat = numpy.mean([Sats[id] for id in sub])
-  meanNZmean = numpy.mean([NZmeans[id] for id in sub])
-  stdSat = numpy.std([Sats[id] for id in sub])
-  stdNZmean = numpy.std([NZmeans[id] for id in sub])
-  print("#  %s: genes=%s, meanSat=%s, stdSat=%s, meanNZmean=%s, stdNZmean=%s" % (st,len(sub),round(meanSat,3),round(stdSat,3),round(meanNZmean,1),round(stdNZmean,1)))
-  meanSats[st] = meanSat
-  meanNZmeans[st] = meanNZmean
-  #stdSats[st] = meanSat # bug
-  #stdNZmeans[st] = meanNZmean # bug
-  stdSats[st] = stdSat # fixed
-  stdNZmeans[st] = stdNZmean # fixed
+  nzmeans = [NZmeans[id] for id in sub]
+  sats = [Sats[id] for id in sub]
 
-def calc_prob(sat,NZmean,meanSat,stdSat,meanNZmean,stdNZmean):
-  a = scipy.stats.norm.pdf(sat,loc=meanSat,scale=stdSat)
-  b = scipy.stats.norm.pdf(NZmean,loc=meanNZmean,scale=stdNZmean)
-  return a*b
+  meanSat = numpy.mean(sats)
+  meanNZmean = numpy.mean(nzmeans)
+  stdSat = numpy.std(sats)
+  stdNZmean = numpy.std(nzmeans)
+  medNZmeans = numpy.median(nzmeans)
+  iqrNZmeans = scipy.stats.iqr(nzmeans)
+
+  # model NZmean with robust Normal distribution: use median and IQR
+  sigma = 0.7413*iqrNZmeans
+  sigma = max(0.01,sigma) # don't let it collapse to 0
+  NZmeanParams[st] = (medNZmeans,sigma)
+  sys.stderr.write("sigma=%s\n" % sigma)
+
+  # model saturation as Beta distribution, fit by method of moments:
+  # https://real-statistics.com/distribution-fitting/method-of-moments/method-of-moments-beta-distribution/
+  alpha = meanSat*( (meanSat*(1.0-meanSat)/(stdSat*stdSat) - 1.0) )
+  beta = alpha*(1.0-meanSat)/meanSat
+  SatParams[st] = (alpha,beta)
+
+  Info.append("#   Sat[%s]:    Beta(alpha=%s,beta=%s), E[sat]=%s" % (st,round(alpha,2),round(beta,2),round(alpha/(alpha+beta),3)))
+  Info.append("#   NZmean[%s]: Norm(mean=%s,stdev=%s)" % (st,round(NZmeanParams[st][0],2),round(NZmeanParams[st][1],2)))
+
+
+def normalize(L):
+  tot = sum(L)
+  return [x/float(tot) for x in L]
+
+# for all 4 states; uses globals
+
+def calc_probs3(sats,nzmean):
+  A,B = [],[]
+  for st in STATES:
+    alpha,beta = SatParams[st]
+    meanNZmean,stdNZmean = NZmeanParams[st]
+    A.append(scipy.stats.beta.pdf(sat,a=alpha,b=beta))
+    B.append(scipy.stats.norm.pdf(nzmean,loc=meanNZmean,scale=stdNZmean))
+  A = normalize(A)
+  B = normalize(B)
+  probs = [a*b for a,b in zip(A,B)]
+  return normalize(probs)
+
+##################################
 
 # second pass...
 
-for line in headers[:-1]: print(line)
-newheaders = headers[-1].split('\t') # assume last comment line is column headers
-newheaders += "consis probES probGD probNE probGA conf flag".split()
-print('\t'.join(newheaders))
-
+num_low_conf,num_ambig = 0,0
+results = []
 for line in open(sys.argv[1]):
   line = line.strip()
   if line[0]=='#': continue
   w = line.split('\t')
   id,Call = w[0],w[-1]
   nTA = int(w[3])
-  if nTA==0: print (line); continue
+  if nTA==0: continue; # print (line); continue
   votes = [int(x) for x in w[4:8]]
   m = max(votes)
   consistency = m/float(nTA)
-  probs = []
-  STATES = "ES GD NE GA".split()
-  for st in STATES:
-    probs.append(calc_prob(float(w[-3]),float(w[-2]),meanSats[st],stdSats[st],meanNZmeans[st],stdNZmeans[st]))
-  totprob = sum(probs)
-  relprobs = [x/float(totprob) for x in probs]
-  conf = relprobs[STATES.index(Call)]
+  sat,NZmean = float(w[-3]),float(w[-2])
+  PC = 0.01 # shrink range of saturation form [0,1] to [0.01,0.99] to prevent nan's from Beta
+  sat = max(PC,min(1.0-PC,sat))
+  probs = calc_probs3(sat,NZmean) # normalized
+  conf = probs[STATES.index(Call)]
   flag=""
   if conf<0.5: flag="low-confidence"
-  if max(relprobs)<0.7:
-    if (Call=="ES" or Call=="GD") and (relprobs[0]>0.25 and relprobs[1]>0.25): flag="ambiguous"
-    if (Call=="GD" or Call=="NE") and  (relprobs[1]>0.25 and relprobs[2]>0.25) :flag="ambiguous"
-    if (Call=="NE" or Call=="GA") and (relprobs[2]>0.25 and relprobs[3]>0.25):flag="ambiguous"
+  if max(probs)<0.7:
+    if (Call=="ES" or Call=="GD") and (probs[0]>0.25 and probs[1]>0.25): flag="ambiguous"
+    if (Call=="GD" or Call=="NE") and  (probs[1]>0.25 and probs[2]>0.25): flag="ambiguous"
+    if (Call=="NE" or Call=="GA") and (probs[2]>0.25 and probs[3]>0.25): flag="ambiguous"
+  if flag=="ambiguous": num_ambig += 1
+  if flag=="low-confidence": num_low_conf += 1
   
-  vals = w+[round(consistency,3)]+[round(x,6) for x in relprobs]
+  vals = w+[round(consistency,3)]+[round(x,6) for x in probs]
   vals += [round(conf,4),flag]
+  results.append(vals)
+
+Info.append("# num low-confidence genes=%s, num ambiguous genes=%s" % (num_low_conf,num_ambig))
+
+###############
+# print results
+
+for line in headers[:-1]: print(line)
+for line in Info: print(line)
+newheaders = headers[-1].split('\t') # assume last comment line is column headers
+newheaders += "consis probES probGD probNE probGA conf flag".split()
+print('\t'.join(newheaders))
+
+for vals in results:
   print ('\t'.join([str(x) for x in vals]))
 
